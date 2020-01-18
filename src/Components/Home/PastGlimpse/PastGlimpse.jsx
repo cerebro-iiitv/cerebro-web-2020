ca344e8cf21e1e71ee4f4fb247e75b4860288e03
Aditya Prakash <p.aditya.198@gmail.com>
2020-01-19 03:57:32 +0530

    frame complete

--- src/Components/Home/PastGlimpse/PastGlimpse.jsx
+++ src/Components/Home/PastGlimpse/PastGlimpse.jsx
@@ -4,7 +4,8 @@ import './PastGlimpse.scss'
 function PastGlimpse() {
     return (
         <div className="container">
-            <h1>PAST YEAR GLIMPSES</h1>
+            <h1>PAST YEAR GLIMPSES</h1><span className="dot"></span>
+            <span className="dot"></span>
             <div className="frame">
             </div>
         </div>
