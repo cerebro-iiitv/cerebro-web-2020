cc1aeee2a4f6210fe098fa8d6026025f5f2a6668
Jay Mistry <mistry.jay15@gmail.com>
2020-01-18 23:31:21 +0530

    Init :rocket:

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+      </div>
+    )
+  }
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './Components/Home/Home'
+import './scss/_base.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
+, document.getElementById('root'))
