13c62e8bf89071177bb83ec6bf64f5fbed7f0e4e
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-01-19 02:46:53 +0530

    Navigation bar initiated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Header.scss'
 
 function Header() {
     return (
         <div>
-            Header
+            <nav className="nav-bar">
+               <ul className="main-nav">
+                    <li ><a href="">HOME</a></li>
+                    <li ><a href="">EVENTS</a></li>
+                    <li ><a href="">TEAM</a></li>
+                    <li ><a href="">TIMELINE</a></li>
+                    <li ><a href="">BROCHURE</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
