fd7c424b8b5e79f2a047b1b204a103d27eb7bb16
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-01-19 14:39:00 +0530

    jai shree Ram

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,15 +4,20 @@ import './Header.scss'
 function Header() {
     return (
         <div>
-            <nav className="nav-bar">
-               <ul className="main-nav">
-                    <li ><a href="">HOME</a></li>
-                    <li ><a href="">EVENTS</a></li>
-                    <li ><a href="">TEAM</a></li>
-                    <li ><a href="">TIMELINE</a></li>
-                    <li ><a href="">BROCHURE</a></li>
-                </ul>
-            </nav>
+            <div>
+                <nav className="nav-bar">
+                {/* <img className="shft-left" src="media/h_logo_1.png"></img> */}
+                <ul className="main-nav">
+                    <li><img className="shft-left" src="media/h_logo_1.png"></img></li>
+                        <li ><a className="dropdown" href="">HOME</a></li>
+                        <li ><a className="dropdown" href="">EVENTS</a></li>
+                        <li ><a className="dropdown" href="">TEAM</a></li>
+                        <li ><a className="dropdown" href="">TIMELINE</a></li>
+                        <li ><a className="dropdown" href="">BROCHURE</a></li>
+                    </ul>
+                    
+                </nav>
+            </div>    
         </div>
     )
 }
