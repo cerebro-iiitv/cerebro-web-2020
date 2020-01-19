b5a5e6fd143cc553d68fe7b5a8de409bd1cb5bc8
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-01-19 15:58:09 +0530

    Header and about section complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,12 +8,13 @@ function Header() {
                 <nav className="nav-bar">
                 {/* <img className="shft-left" src="media/h_logo_1.png"></img> */}
                 <ul className="main-nav">
-                    <li><img className="shft-left" src="media/h_logo_1.png"></img></li>
+                    <li><img className="shift-left" src="media/h_logo_1.png"></img></li>
                         <li ><a className="dropdown" href="">HOME</a></li>
                         <li ><a className="dropdown" href="">EVENTS</a></li>
                         <li ><a className="dropdown" href="">TEAM</a></li>
                         <li ><a className="dropdown" href="">TIMELINE</a></li>
                         <li ><a className="dropdown" href="">BROCHURE</a></li>
+                        <li><button class="btn-round"><img class="g_img" src="media/google.png" alt=""/></button></li>
                     </ul>
                     
                 </nav>
