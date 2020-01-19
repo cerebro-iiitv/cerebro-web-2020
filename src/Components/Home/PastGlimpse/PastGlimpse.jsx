98e6e70ee117597bb065d0c789a723a956ac90d0
paditya198 <p.aditya.198@gmail.com>
2020-01-19 14:46:12 +0530

    PastGlimpse Completed

--- src/Components/Home/PastGlimpse/PastGlimpse.jsx
+++ src/Components/Home/PastGlimpse/PastGlimpse.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './PastGlimpse.scss'
+import React from "react";
+import "./PastGlimpse.scss";
 
 function PastGlimpse() {
-    return (
-        <div className="container">
-            <h1>PAST YEAR GLIMPSES</h1><span className="dot"></span>
-            <span className="dot"></span>
-            <div className="frame">
-            </div>
+  return (
+    <div className="container">
+      <div className="inner-container">
+        <h1>PAST YEAR GLIMPSES</h1>
+        <span className="dot"></span>
+        <div className="frame">
+            <img src="media/pastGlimpse/img1.jpeg" className="image" />
         </div>
-    )
+        </div><div>
+        <img src="media/pastGlimpse/Group 55.png" className="group"></img>
+        <img src="media/pastGlimpse/Ellipse 32.png" className="circle1"></img>
+        <img src="media/pastGlimpse/Ellipse 33.png" className="circle2"></img>
+        <img src="media/pastGlimpse/Rectangle 29.png" className="rec1"></img>
+        <img src="media/pastGlimpse/Rectangle 30.png" className="rec2"></img>
+      </div>
+    </div>
+  );
 }
 
-export default PastGlimpse
+export default PastGlimpse;
