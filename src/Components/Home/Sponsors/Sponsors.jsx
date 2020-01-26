ab4275a3c7b6cbf5ca00aba338621ad52b838460
Mayank Ajmeri <mayankajmeri@gmail.com>
2020-01-26 20:02:54 +0530

    Refactored code

--- src/Components/Home/Sponsors/Sponsors.jsx
+++ src/Components/Home/Sponsors/Sponsors.jsx
@@ -4,9 +4,9 @@ import "../../../Components/Home/Sponsors/Sponsors.scss";
 function Sponsors() {
   return (
     <div className="sponsor_container">
-      <div className="cont">
+      <div className="sponsor_text_container">
         <div className="hr-container">
-          <div className="hr-txt">
+          <div className="hr-txt__line1">
             <hr color="#29abe2" />
           </div>
           <div className="hr-txt2">
@@ -15,7 +15,7 @@ function Sponsors() {
         </div>
         <div className="sponsors_text">SPONSORS</div>
         <div className="hr-container">
-          <div className="hr-txt3">
+          <div className="hr-txt">
             <hr color="#29abe2" />
           </div>
           <div className="hr-txt2">
@@ -26,22 +26,22 @@ function Sponsors() {
       <div class="sponsor_wrapper">
         <img
           src="media/stickermuletransparent.png"
-          className="sponsor_one"
+          className="sponsor_img"
           alt="sponsor One"
         />
         <img
           src="media/souledstoretransparent.png"
-          className="sponsor_two"
+          className="sponsor_img"
           alt="sponsor Two"
         />
         <img
           src="media/stickermuletransparent.png"
-          className="sponsor_three "
+          className="sponsor_img"
           alt="sponsor Three"
         />
         <img
           src="media/souledstoretransparent.png"
-          className="sponsor_four "
+          className="sponsor_img"
           alt="sponsor Four"
         />
       </div>
