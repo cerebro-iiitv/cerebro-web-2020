b3ae724c7260fcaf3ae9fbc015e8f8b57d5e712a
Vraj Mistry <vrajparesh22@gmail.com>
2020-01-30 00:08:56 +0530

    Footer refined

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,38 +3,64 @@ import './Footer.scss';
 
 
 function Footer() {
-    return (
-        <div>
-            <div className="hr-footer-container"><hr className="hr-line" color="grey" /></div>
-            <div className="footer-container">
+	return (
+		// <div>
+		// 	<div className="hr-footer-container"><div></div><div className="hr-footer"></div><div></div></div>
+		// 	<div className="footer-container">
 
-                <div className="logo">
-                    <div>
-                        <img src='/media/institute-logo.1fb6100a.svg' className="institute-logo" alt="IIIT Vadodara Logo" />
-                    </div>
-                    <div className="span">Indian Institute of Information Technology, Vadodara</div>
-                </div>
-                {/* <div></div> */}
-                <div className="links">
-                    <div>
-                        <div className="footer-lc-title" ><h2>Links</h2><div className="hr-text"><hr color="#25cbc0" /></div><br /><br/><br /></div>
-                    </div>
-                    <div className="links-div"><a class="linkname" >Home</a></div>
-                    <div className="links-div"><a class="linkname" >Institute Site</a></div>
-                    <div className="links-div"><a class="linkname" >Events</a></div>
-                    <div className="links-div"><a class="linkname" >Timeline</a></div>
-                </div>
-                <div className="contact">
-                    <div>
-                        <div className="footer-lc-title" ><h2>Contact</h2><div className="hr-text"><hr color="#25cbc0" /></div><br /><br/><br /></div>
-                    </div>
-                    <div className="links-div"><a class="linkname" >Email: cerebro@iiitv.ac.in</a></div>
-                    <div className="links-div"><a class="linkname" >Phone: +91-9370645038</a></div>
-                    <div className="links-div"><a class="linkname" >Address: c/o Block No.9, Government Engineering<div className="links_map"> College, Sector-28, Gandhinagar, Gujarat - 382028</div></a></div>
-                </div>
-            </div>
-        </div>
-    );
+		// 		<div className="logo">
+		// 			<div>
+		// 				<img src='/media/institute-logo.1fb6100a.svg' className="institute-logo" alt="IIIT Vadodara Logo" />
+		// 			</div>
+		// 			<div className="span">Indian Institute of Information Technology, Vadodara</div>
+		// 		</div>
+		// 		<div className="links">
+		// 			<div>
+		// 				<div className="footer-lc-title" ><h2>Links</h2><div className="hr-text"><hr color="#25cbc0" /></div><br /><br /><br /></div>
+		// 			</div>
+		// 			<div className="footer__links_content"><a className="footer__links_cn" >Home</a></div>
+		// 			<div className="footer__links_content"><a class="footer__links_cn" >Institute Site</a></div>
+		// 			<div className="footer__links_content"><a class="footer__links_cn" >Events</a></div>
+		// 			<div className="footer__links_content"><a class="footer__links_cn" >Timeline</a></div>
+		// 		</div>
+		// 		<div className="contact">
+		// 			<div>
+		// 				<div className="footer-lc-title" ><h2>Contact</h2><div className="hr-text"><hr color="#25cbc0" /></div><br /><br /><br /></div>
+		// 			</div>
+		// 			<div className="footer__links_content"><a class="footer__links_cn" >Email: cerebro@iiitv.ac.in</a></div>
+		// 			<div className="footer__links_content"><a class="footer__links_cn" >Phone: +91-9370645038</a></div>
+		// 			<div className="footer__links_content"><a class="footer__links_cn" >Address: c/o Block No.9, Government Engineering<div className="links_map"> College, Sector-28, Gandhinagar, Gujarat - 382028</div></a></div>
+		// 		</div>
+		// 	</div>
+		// </div>
+		<div className="footer">
+			<div className="footer__logo">
+				<div>
+					<img src='/media/institute-logo.1fb6100a.svg' className="footer__logo_svg" alt="IIIT Vadodara Logo" />
+				</div>
+				<div className="footer__logo_span">Indian Institute of Information Technology, Vadodara</div>
+			</div>
+
+			<div className="footer__links">
+				<div>
+					<div className="footer__hr" ><h2>Links</h2><div className="footer__hr_text"><hr color="#25cbc0" /></div><br /><br /><br /></div>
+				</div>
+				<div className="footer__links_content"><a className="footer__links_cn" >Home</a></div>
+				<div className="footer__links_content"><a className="footer__links_cn" >Institute Site</a></div>
+				<div className="footer__links_content"><a className="footer__links_cn" >Events</a></div>
+				<div className="footer__links_content"><a className="footer__links_cn" >Timeline</a></div>
+			</div>
+			<div className="footer__contact">
+				<div>
+					<div className="footer__hr" ><h2>Contact</h2><div className="footer__hr_text"><hr color="#25cbc0" /></div><br /><br /><br /></div>
+				</div>
+				<div className="footer__links_content"><a className="footer__links_cn" >Email: cerebro@iiitv.ac.in</a></div>
+				<div className="footer__links_content"><a className="footer__links_cn" >Phone: +91-9370645038</a></div>
+				<div className="footer__links_content"><a className="footer__links_cn" >Address: c/o Block No.9, Government Engineering<div className="footer__gmap"> College, Sector-28, Gandhinagar, Gujarat - 382028</div></a></div>
+
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
