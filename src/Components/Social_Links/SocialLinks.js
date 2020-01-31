78d47e32f01d25e1e8b3b696ce03bb023e72d2df
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-02-01 01:22:57 +0530

    social Links finished

---
+++ src/Components/Social_Links/SocialLinks.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+import '../Social_Links/SocialLinks.scss'
+
+const SocialLinks = () => {
+    return (
+        <div>
+            <ul className="font-awe"> 
+               <div className="font-awe__verticalLine">
+                   <li className="ListElement "></li>
+               </div> 
+                <li ><a><i className="ListElement fa fa-instagram fa-2x"></i></a></li>
+                <li ><a><i className="ListElement fa fa-twitter fa-2x"></i></a></li>
+                <li ><a><i className="ListElement fa fa-facebook fa-2x"></i></a></li>
+            </ul>    
+        </div>
+    );
+
+}
+export default SocialLinks;
