c20e6ad27805d20f01a3291c7ec56c2c5db88214
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2020-02-20 00:42:41 +0530

    up to date (#9)
    * Favicon Inserted
    
    * changed scroll
    
    * hamburger finaliszed
    
    * fixed abou section mobileview
    
    * small fix sponsor section
    
    * Past-Glimpse slider update
    
    * Create main.yml
    
    * Update README.md
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    Co-authored-by: eshaan007 <42370985+eshaan007@users.noreply.github.com>
    Co-authored-by: Anuj Puri <anujpuri72@gmail.com>
    Co-authored-by: Jay Mistry <mistry.jay15@gmail.com>
    Co-authored-by: prathmeshkhandelwal <55102206+prathmeshkhandelwal@users.noreply.github.com>
    Co-authored-by: Aman Raj <42104907+AmanRaj1608@users.noreply.github.com>

--- src/Components/Home/PastGlimpse/PastGlimpse.jsx
+++ src/Components/Home/PastGlimpse/PastGlimpse.jsx
@@ -1,90 +1,112 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./PastGlimpse.scss";
 
-function PastGlimpse() {
 
-  let count = 0;
+class PastGlimpse extends Component {
+  constructor(props) {
+    super(props);
 
-  let array = ['1', '2', '3', '4', '5', '6', '7', '8'];
-  let images = array.map(image => {
-    return <img key={image} src={`https://picsum.photos/50/50`} alt="" className="past-glimpse" />
-  });
+    this.state = {
+      active: 0,
+      count: 0,
+      images: [
+        'media/pastGlimpse/img1.jpeg',
+        'media/pastGlimpse/Group 55.png',
+        'media/Asset 12@4x.png',
+        'media/stickermuletransparent.png',
+        'media/souledstoretransparent.png',
+        'media/pastGlimpse/img1.jpeg',
+        'media/Asset 12@4x.png',
+        'media/pastGlimpse/img1.jpeg'
+      ]
+    }
+  }
 
 
-  return (
-    <div className="past-glimpse">
+  componentDidMount() {
+    this.slideImages();
+  }
 
-      <div className="past-glimpse__container">
-        <div className="past-glimpse__container__heading">
-          PAST YEAR GLIMPSES
+  slideImages = () => {
+    this.interval = setInterval(() => {
+      this.setState({
+        active: this.state.active + 1
+      })
+    }, 3000);
+  }
+
+  
+
+
+  slideshow() {
+    return this.state.images.map((image,index) => {
+      return <div className="past-glimpse__container__slideshow__list__des__items">
+        <img key={this.state.active} src={`${image}`} alt={index}
+          className="past-glimpse__container__slideshow__list__des__items__images" onClick={this.imageClick} />
+      </div>
+    });
+  }
+
+  imageClick = (e) => {
+    let ok = e.target.alt%7
+    this.setState({ active: ok });
+  }
+
+  render() {
+    const slide = this.slideshow();
+    return (
+      <div className="past-glimpse" >
+
+        <div className="past-glimpse__container">
+          <div className="past-glimpse__container__heading">
+            PAST YEAR GLIMPSES
         </div>
-        <div className="past-glimpse__container__slideshow">
-          <img
-            src="media/pastGlimpse/img1.jpeg"
-            className="past-glimpse__container__slideshow__slide"
-            alt="slideshow"
-          ></img>
-        </div>
-
-        <div className="past-glimpse__container__slideshow__list">
-          <div className="past-glimpse__container__slideshow__list__des">
-            <div className="past-glimpse__container__slideshow__list__des__items">
-              <img
-                src="https://picsum.photos/80/80"
-                className="past-past-glimpse__container__slideshow__list__des__items__images"
-                alt="slideshow"
-              ></img>
-            </div>
-            <div className="past-glimpse__container__slideshow__list__des__items">
-            </div>
-            <div className="past-glimpse__container__slideshow__list__des__items">
-            </div>
-            <div className="past-glimpse__container__slideshow__list__des__items">
-            </div>
-            <div className="past-glimpse__container__slideshow__list__des__items">
-            </div>
-            <div className="past-glimpse__container__slideshow__list__des__items">
-            </div>
-            <div className="past-glimpse__container__slideshow__list__des__items">
-            </div>
-            <div className="past-glimpse__container__slideshow__list__des__items">
-            </div>
-
+          <div className="past-glimpse__container__slideshow">
+            <img
+              src={this.state.images[this.state.active % this.state.images.length]}
+              className="past-glimpse__container__slideshow__slide"
+              alt="slideshow"
+            ></img>
           </div>
 
+          <div className="past-glimpse__container__slideshow__list">
+            <div className="past-glimpse__container__slideshow__list__des">
+              {slide}
+            </div>
+          </div>
+        </div>
+
+
+        <div className="past-glimpse__images">
+          <img
+            src="media/pastGlimpse/Ellipse 32.png"
+            className="past-glimpse__images__elli1"
+            alt="elli1"
+          ></img>
+          <img
+            src="media/pastGlimpse/Ellipse 33.png"
+            className="past-glimpse__images__elli2"
+            alt="elli2"
+          ></img>
+          <img
+            src="media/pastGlimpse/Rectangle 29.png"
+            className="past-glimpse__images__rec1"
+            alt="rec1"
+          ></img>
+          <img
+            src="media/pastGlimpse/Rectangle 30.png"
+            className="past-glimpse__images__rec2"
+            alt="rec2"
+          ></img>
+          <img
+            src="media/pastGlimpse/Group 55.png"
+            className="past-glimpse__images__group"
+            alt="group"
+          ></img>
         </div>
       </div>
-
-
-      <div className="past-glimpse__images">
-        <img
-          src="media/pastGlimpse/Ellipse 32.png"
-          className="past-glimpse__images__elli1"
-          alt="elli1"
-        ></img>
-        <img
-          src="media/pastGlimpse/Ellipse 33.png"
-          className="past-glimpse__images__elli2"
-          alt="elli2"
-        ></img>
-        <img
-          src="media/pastGlimpse/Rectangle 29.png"
-          className="past-glimpse__images__rec1"
-          alt="rec1"
-        ></img>
-        <img
-          src="media/pastGlimpse/Rectangle 30.png"
-          className="past-glimpse__images__rec2"
-          alt="rec2"
-        ></img>
-        <img
-          src="media/pastGlimpse/Group 55.png"
-          className="past-glimpse__images__group"
-          alt="group"
-        ></img>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default PastGlimpse;
