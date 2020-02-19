d221c9ef185e9b579c004314b79e9ae6b9057814
Aman Raj <archanaamanraj@gmail.com>
2020-02-20 00:45:39 +0530

    event page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './Components/Home/Home'
+import Events from './Components/Events/Events'
 import './scss/_base.scss'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/events" component={Events} />
     </Switch>
   </BrowserRouter>
 , document.getElementById('root'))
