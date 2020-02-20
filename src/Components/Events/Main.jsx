3d8d2ae1510217eb040bc1727d0444e2dcc59d99
Aman Raj <archanaamanraj@gmail.com>
2020-02-20 21:16:52 +0530

    event main updated

--- src/Components/Events/Main.jsx
+++ src/Components/Events/Main.jsx
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
 import "./Main.scss";
 
-class Main extends Component {
-
-    render() {
+const Main = props => {
+    const MainList = props.events.map((event) => {
         return (
-            <div className="main" >
-                <div className="main__container">
-                    <h1 className="main__container__title">COOK-A-CODE</h1>
-                    <div className="main__container__content">
-                        <div>
-                            <div>
-                                Let the programming juice flow
-                                in your brain!
+            <div className="main__container__content__left__description">
+                {event.description}
+            </div>
+        )
+    })
+    return (
+        <div className="main" >
+            <div className="main__container">
+                <h1 className="main__container__title">COOK-A-CODE</h1>
+                <div className="main__container__content">
+
+                    <div className="main__container__content__left">
+                        <div className="main__container__content__left__description">
+                            Let the programming juice flow
+                            in your brain!
+                        </div>
+                        <div className="main__container__content__left__info">
+                            <div className="main__container__content__left__details">
+                                <p className="main__container__content__left__info__colorchange">Prize Worth: </p> 6000
+                            </div>
+                            <div className="main__container__content__left__details">
+                                <p className="main__container__content__left__info__colorchange">Team Size: </p> 3
+                            </div>
+                            <div className="main__container__content__left__details">
+                                <p className="main__container__content__left__info__colorchange">Venue: </p> 9102 (A and B)
+                            </div>
+                            <div className="main__container__content__left__details">
+                                <p className="main__container__content__left__info__colorchange">Time: </p> 2019-03-03 03:30PM to
+                                2019-03-03 06:30PM
                             </div>
                         </div>
-                        <span class="vl"></span>
-                        <div>• Both the rounds online as well as
-                        onsite will follow the ACM style
+                    </div>
+
+                    <span class="main__container__content__vl"></span>
+
+                    <div className="main__container__content__right">
+                        <div className="main__container__content__right__rules">
+                            • Both the rounds online as well as
+                            onsite will follow the ACM style
                         ranking system <br />
 
                             • Any type of plagiarism detected
@@ -26,12 +51,17 @@ class Main extends Component {
 
                             • A team must contain at most 3
                         members.  <br />
+
                         </div>
+
+                        <div className="main__container__content__right__reg">Rules and Regulations</div>
                     </div>
+
                 </div>
+                <button class="main__container__button">Register</button>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Main;
