074f53c4b440260104b712cc820f9d93643e2acb
Aman Raj <archanaamanraj@gmail.com>
2020-02-20 17:40:54 +0530

    main hardcoded

--- src/Components/Events/Main.jsx
+++ src/Components/Events/Main.jsx
@@ -9,9 +9,24 @@ class Main extends Component {
                 <div className="main__container">
                     <h1 className="main__container__title">COOK-A-CODE</h1>
                     <div className="main__container__content">
-                        <div>Left side hai</div>
+                        <div>
+                            <div>
+                                Let the programming juice flow
+                                in your brain!
+                            </div>
+                        </div>
                         <span class="vl"></span>
-                        <div>Right side hai</div>
+                        <div>• Both the rounds online as well as
+                        onsite will follow the ACM style
+                        ranking system <br />
+
+                            • Any type of plagiarism detected
+                              will lead to immediate
+                        disqualification  <br />
+
+                            • A team must contain at most 3
+                        members.  <br />
+                        </div>
                     </div>
                 </div>
             </div>
