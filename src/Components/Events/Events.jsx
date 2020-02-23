23e58f2627763fdd0052539bf28336663da10637
anujpuri72 <anujpuri72@gmail.com>
2020-02-24 03:04:37 +0530

    added order on events page

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,25 +1,45 @@
 import React, { Component } from "react";
 
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Sidelist from "./Sidelist";
-import Main from "./Main"/ain;
+import Sidelist from "./Sidelist/Sidelist";
+import Main from "./Main/Main";
 import "./Events.scss";
+import { events } from "../Events/util/event";
+import Circle from "./Circle/Circle";
 class Events extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eventIndex: 0
+    };
+  }
+
+  updateEvent = index => {
+    this.setState({
+      eventIndex: index
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
         <div className="event">
-          <div>Circle</div>
           <div>
-            <Main />
+            <Circle />{" "}
           </div>
           <div>
-            <Sidelist />
+            <Main event={events[this.state.eventIndex]} />
+          </div>
+          <div>
+            <Sidelist
+              events={events}
+              updateEvent={this.updateEvent}
+              index={this.state.eventIndex}
+            />
           </div>
         </div>
-        <Footer />
       </div>
     );
   }
