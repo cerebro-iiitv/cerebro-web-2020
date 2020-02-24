7715a8f1725932cb405fa099e3c1b1d614c0439b
Aditya Prakash <p.aditya.198@gmail.com>
2020-02-25 00:29:36 +0530

    minor changes for events page

--- src/Components/Team/util/Teamdata.js
+++ src/Components/Team/util/Teamdata.js
@@ -1,18 +1,18 @@
  export const teamdata = {
 
-  'Core Team': [
+  'Core': [
     { id: 1, 'name': 'Keshav Purohit', 'role': 'Student Convenor', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Ananya Tewari', 'role': 'Technical Lead', 'img': 'http://unsplash.it/230/230' }
   ],
 
-  'Core Support Team': [
+  'Core Support': [
     { id: 1, 'name': 'Dipti Solanki', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Harsh Kakani', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
     { id: 3, 'name': 'Vraj Misrty', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
     { id: 4, 'name': 'Shivam Balwani', 'role': 'Member', 'img': 'http://unsplash.it/230/230' }
   ],
 
-  'Web Dev Team': [
+  'Web Dev': [
     { id: 1, 'name': 'Jay Mistry', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Samagra Gupta', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
     { id: 3, 'name': 'Yash Shah', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75118602-65734d00-56a1-11ea-91c1-d77d63237157.jpg' },
@@ -22,7 +22,7 @@
     { id: 6, 'name': 'Mayank Ajmeri', 'role': 'Member', 'img': 'http://unsplash.it/230/230' }
   ],
 
-  'Design & Video Team': [
+  'Design & Video': [
     { id: 1, 'name': 'Pranay Agarwal ', 'role': 'Design Head', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Vikash Kumar', 'role': 'Video Head', 'img': 'http://unsplash.it/230/230' },
     { id: 3, 'name': 'Kavya Tripathi', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
@@ -38,23 +38,23 @@
     { id: 12, 'name': 'Deepanshu Singh', 'role': 'Member', 'img': 'http://unsplash.it/230/230' }
   ],
 
-  'Sponsorship Team': [
+  'Sponsorship': [
     { id: 1, 'name': 'Abhijeet Rathore', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Tanmay Ambadkar', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' }
   ],
 
-  'Publicity and Marketing Team': [
+  'Publicity and Marketing': [
     { id: 1, 'name': 'Aayush Dheemar', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Anubhav Madhav', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
     { id: 3, 'name': 'Anagha Mittal', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' }
   ],
 
-  'Gaming Team': [
+  'Gaming': [
     { id: 1, 'name': 'Ravi Varshney', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Shivam Nigam', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' }
   ],
 
-  'Art & Infrastructure Team': [
+  'Art & Infrastructure': [
     { id: 1, 'name': 'Srishti Chaudhary', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
     { id: 2, 'name': 'Shilp Khatri', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
     { id: 3, 'name': 'Jinal Adhyaru', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
