e5bb1de12dbd6d5a764e5d52d48ece4e45b19614
Mayank Ajmeri <48920341+MayankAjmeri@users.noreply.github.com>
2020-02-26 18:52:38 +0530

    Updated components in about section (#54)
    * Updated Sponsors for small screen resolution
    
    * Updated download button in Timeline
    
    * Removed download button bug
    
    * updated components in about section
    
    * Removed comments
    
    * Elements are moved above the image in about section

--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -6,11 +6,6 @@ function About() {
     <div className="aboutcontainer">
       <div className="aboutcontainer__images">
         <img
-          src="media/Asset 12@4x.png"
-          className="aboutcontainer__images__spaceship"
-          alt="spaceship"
-        />
-        {/* <img
           src="media/Asset 16@4x.png"
           className="aboutcontainer__images__Recpurplebig animation-particles-rotate"
           alt="rectanglepurple"
@@ -34,7 +29,22 @@ function About() {
           src="media/Asset 15@4x.png"
           className="aboutcontainer__images__Circpurple animation-particles-rotate"
           alt="circpurple"
-        /> */}
+        />
+        <img
+          src="media/planet-elements-06.svg"
+          className="aboutcontainer__images__spaceship1"
+          alt="spaceship"
+        />
+        <img
+          src="media/planet-elements-05.svg"
+          className="aboutcontainer__images__spaceship2"
+          alt="spaceship"
+        />
+        <img
+          src="media/planet-elements-07.svg"
+          className="aboutcontainer__images__spaceship3"
+          alt="spaceship"
+        />
       </div>
       <div className="aboutcontainer__card">
         <div className="aboutcontainer__card__header section-title">
