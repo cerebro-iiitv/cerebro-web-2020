6c99c0e78cdacc42742d38f88ea90ad05d1189f3
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-02-26 23:00:49 +0530

    Sponsors Section Updated and Past Glimpse images added (#53)
    * Sponsors Section Updated
    
    * Past Glimpses: Images added
    
    * Past Glimpses : Mobile View

--- src/Components/Home/PastGlimpse/PastGlimpse.jsx
+++ src/Components/Home/PastGlimpse/PastGlimpse.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import './PastGlimpse.scss'
 
 const PastGlimpse = () => {
+  const width=window.screen.width;
   return (
     <div className="gallery">
       <h1 className="gallery__title section-title">Past year Glimpses</h1>
       <div className="gallery__images">
         <div className="gallery__images__container">
-          <div className="wide" style={{backgroundImage: `url('https://unsplash.it/seed/10/1280/720')`}}></div>
-          <div className="tall" style={{backgroundImage: `url('https://unsplash.it/seed/20/1280/720')`}}></div>
-          <div className="big" style={{backgroundImage: `url('https://unsplash.it/seed/30/1280/720')`}}></div>
-          <div className="wide" style={{backgroundImage: `url('https://unsplash.it/seed/40/1280/720')`}}></div>
-          <div className="tall" style={{backgroundImage: `url('https://unsplash.it/seed/50/1280/720')`}}></div>
-          <div className="" style={{backgroundImage: `url('https://unsplash.it/seed/60/1280/720')`}}></div>
-          <div className="wide" style={{backgroundImage: `url('https://unsplash.it/seed/70/1280/720')`}}></div>
-          <div className="" style={{backgroundImage: `url('https://unsplash.it/seed/80/1280/720')`}}></div>
+          <div className="wide" style={{backgroundImage:`url('./media/botrun${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="tall" style={{backgroundImage: `url('./media/git${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="big" style={{backgroundImage: `url('./media/team${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="wide" style={{backgroundImage: "url('./media/ideathon.png')"}}></div>
+          <div className="tall" style={{backgroundImage: "url('./media/director.png')"}}></div>
+          <div className="" style={{backgroundImage: `url('./media/penumbera${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="wide" style={{backgroundImage: `url('./media/baymax${width<=1000?'_mobile':''}.png')`}}></div>
+          <div className="" style={{backgroundImage: `url('./media/webhead${width<=1000?'_mobile':''}.png')`}}></div>
         </div>
       </div>
     </div>
