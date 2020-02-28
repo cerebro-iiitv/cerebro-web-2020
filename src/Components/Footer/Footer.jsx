0f102dd32bac69895814aae3bf63f4a371469ecd
Vraj Mistry <vrajparesh22@gmail.com>
2020-02-28 22:27:47 +0530

    Footer Links added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.scss';
+import { NavLink } from 'react-router-dom';
 
 function Footer() {
 	return (
@@ -21,10 +22,10 @@ function Footer() {
 						<div className="footer_l__hr_text"><hr color="#29abe2" /></div>
 						<br /><br /><br /></div>
 				</div>
-				<div className="footer_l__links_content"><a className="footer_l__links_cn" >Home</a></div>
-				<div className="footer_l__links_content"><a className="footer_l__links_cn" >Institute Site</a></div>
-				<div className="footer_l__links_content"><a className="footer_l__links_cn" >Events</a></div>
-				<div className="footer_l__links_content"><a className="footer_l__links_cn" >Timeline</a></div>
+					<div className="footer_l__links_content"><NavLink className="footer_l__links_cn" exact to="/">Home</NavLink></div>
+					<div className="footer_l__links_content"><a href='http://iiitvadodara.ac.in/' className="footer_l__links_cn" >Institute Site</a></div>
+					<div className="footer_l__links_content"><NavLink className="footer_l__links_cn" exact to="/events">Events</NavLink></div>
+					<div className="footer_l__links_content"><NavLink className="footer_l__links_cn" exact to="/timeline">Timeline</NavLink></div>
 			</div>
 			<div className="footer_l__contact">
 				<div>
@@ -33,9 +34,9 @@ function Footer() {
 						<h2>Contact</h2>
 						<div className="footer_l__hr_text"><hr color="#29abe2" /></div><br /><br /><br /></div>
 				</div>
-				<div className="footer_l__contact_content"><a className="footer_l__contact_cn" >Email: cerebro@iiitv.ac.in                             </a></div>
-				<div className="footer_l__contact_content"><a className="footer_l__contact_cn" >Phone: +91-9370645038</a></div>
-				<div className="footer_l__contact_content"><a className="footer_l__contact_cn" >Address: c/o Block No.9, Government Engineering<div className="footer_l__contact_gmap">College, Sector-28, Gandhinagar, Gujarat - 382028</div></a></div>
+					<div className="footer_l__contact_content"><a href='mailto:cerebro@iiitvadodara.ac.in' className="footer_l__contact_cn" ><b>Email</b>: cerebro@iiitvadodara.ac.in                             </a></div>
+					<div className="footer_l__contact_content"><a href='tel:8875038111' className="footer_l__contact_cn" ><b>Phone</b>: +91 8875038111,</a><a href='tel:9453286926' className="footer_l__contact_cn" > 9453286926  </a></div>
+					<div className="footer_l__contact_content"><a href='https://goo.gl/maps/7g5D6XJpMAD2' className="footer_l__contact_cn" ><b>Address</b>: c/o Block No.9, Government Engineering<div className="footer_l__contact_gmap">College, Sector-28, Gandhinagar, Gujarat - 382028</div></a></div>
 			</div>
 		</div>
 	</div>
