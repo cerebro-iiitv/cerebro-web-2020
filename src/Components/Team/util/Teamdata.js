f931555aa4701d7da7a2dec08630b2f6e7565625
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-02-28 14:24:59 +0530

    images of team members added

--- src/Components/Team/util/Teamdata.js
+++ src/Components/Team/util/Teamdata.js
@@ -1,65 +1,65 @@
  export const teamdata = {
 
   'Core Team': [
-    { id: 1, 'name': 'Keshav Purohit', 'role': 'Student Convenor', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Ananya Tewari', 'role': 'Technical Lead', 'img': 'http://unsplash.it/230/230' }
+     { id: 1, 'name': 'Keshav Purohit', 'role': 'Student Convenor', 'img': 'https://user-images.githubusercontent.com/42682370/75472030-f0618980-59b8-11ea-8401-0bad421c24fc.jpg' },
+     { id: 2, 'name': 'Ananya Tewari', 'role': 'Technical Lead', 'img': 'https://user-images.githubusercontent.com/42682370/75471846-bdb79100-59b8-11ea-9453-fe4cb2000b77.jpg' }
   ],
 
   'Core Support Team': [
-    { id: 1, 'name': 'Dipti Solanki', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Harsh Kakani', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 3, 'name': 'Vraj Misrty', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 4, 'name': 'Shivam Balwani', 'role': 'Member', 'img': 'http://unsplash.it/230/230' }
+    { id: 1, 'name': 'Dipti Solanki', 'role': 'Head', 'img': 'https://user-images.githubusercontent.com/42682370/75472097-ffe0d280-59b8-11ea-8af4-be1663b0e0e9.JPG' },
+    { id: 2, 'name': 'Harsh Kakani', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75472285-3f0f2380-59b9-11ea-8b13-5c9f46628d5b.jpg' },
+    { id: 3, 'name': 'Vraj Mistry', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75472309-4b937c00-59b9-11ea-8f9e-00cb986a136c.jpg' },
+    { id: 4, 'name': 'Shivam Balwani', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75472324-50f0c680-59b9-11ea-8e38-90daa2f759b3.JPG' },
+    { id: 5, 'name': 'Prateek Chouhan', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75472329-54844d80-59b9-11ea-9461-68f2af1a3431.jpg' }
   ],
 
   'Web Dev Team': [
     { id: 1, 'name': 'Jay Mistry', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Samagra Gupta', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
+    { id: 2, 'name': 'Samagra Gupta', 'role': 'Head', 'img': 'https://user-images.githubusercontent.com/42682370/75472416-78479380-59b9-11ea-8cae-8e92dccd74ff.jpeg' },
     { id: 3, 'name': 'Yash Shah', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75118602-65734d00-56a1-11ea-91c1-d77d63237157.jpg' },
-    { id: 4, 'name': 'Aditya Prakash', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 5, 'name': 'Aman Raj', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 5, 'name': 'Anuj Puri', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 6, 'name': 'Mayank Ajmeri', 'role': 'Member', 'img': 'http://unsplash.it/230/230' }
+    { id: 4, 'name': 'Aditya Prakash', 'role': 'Member', 'img': 'https://avatars3.githubusercontent.com/u/42460356?s=400&v=4' },
+    { id: 5, 'name': 'Aman Raj', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75374655-aca44c80-58f2-11ea-90fe-6a4eb1fe511d.jpg' },
+    { id: 6, 'name': 'Anuj Puri', 'role': 'Member', 'img': 'https://avatars1.githubusercontent.com/u/42184782?s=400&v=4' },
+    { id: 7, 'name': 'Mayank Ajmeri', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75472630-cfe5ff00-59b9-11ea-8c04-30b9902ea48a.jpg' },
+    { id: 8, 'name': 'Eshaan Khurana', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75472645-d3798600-59b9-11ea-8e6b-b5563c7c0ab1.png' }
   ],
 
   'Design & Video Team': [
-    { id: 1, 'name': 'Pranay Agarwal ', 'role': 'Design Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Vikash Kumar', 'role': 'Video Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 3, 'name': 'Kavya Tripathi', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 3, 'name': 'Hardik Soni', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 4, 'name': 'Sahil Kundal', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 5, 'name': 'Saloni Lakhotia', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 6, 'name': 'Amit Prajapati', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 7, 'name': 'Yash Agrawal', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 8, 'name': 'Animesh Gupta', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 9, 'name': 'Vikrant Kumar', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 10, 'name': 'Chirag Mathur', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 11, 'name': 'Vikrant Kumar', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 12, 'name': 'Deepanshu Singh', 'role': 'Member', 'img': 'http://unsplash.it/230/230' }
+    { id: 1, 'name': 'Pranay Agarwal ', 'role': 'Design Head', 'img': 'https://user-images.githubusercontent.com/42682370/75472711-eab87380-59b9-11ea-85b5-76578c758176.jpg' },
+    { id: 2, 'name': 'Vikash Kashyap', 'role': 'Video Head', 'img': 'https://user-images.githubusercontent.com/42682370/75472739-f6a43580-59b9-11ea-8bff-10f300a7f305.jpg' },
+    { id: 3, 'name': 'Kavya Tripathi', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75472814-1dfb0280-59ba-11ea-9eb8-1c7c9532724e.jpg' },
+    { id: 4, 'name': 'Hardik Soni', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75472850-2c491e80-59ba-11ea-8f51-23004b078e3e.jpg' },
+    { id: 5, 'name': 'Sahil Kundal', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75517753-787d7880-5a25-11ea-94f7-336ff3a0cf69.jpg' },
+    { id: 6, 'name': 'Amit Kumar Kumavat', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75473123-9b267780-59ba-11ea-9146-44245d6e48ae.jpg' },
+    { id: 7, 'name': 'Yash Agrawal', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75473182-b3969200-59ba-11ea-8c61-547fbcd6e5a2.jpg' },
+    { id: 8, 'name': 'Animesh Gupta', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75473229-cad57f80-59ba-11ea-8270-cfd428aae1ff.jpg' },
+    { id: 9, 'name': 'Vikrant Kumar', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75473241-cf9a3380-59ba-11ea-9bc2-b46a525d3a98.jpg' },
+    { id: 10, 'name': 'Chirag Mathur', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75473253-d32dba80-59ba-11ea-9b2d-d69b417d2eed.jpg' },
+    { id: 11, 'name': 'Deepanshu Singh', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75473264-d759d800-59ba-11ea-90cd-4e52c824d7c6.jpg' }
   ],
 
   'Sponsorship Team': [
-    { id: 1, 'name': 'Abhijeet Rathore', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Tanmay Ambadkar', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' }
+    { id: 1, 'name': 'Abhijeet Singh Rathore', 'role': 'Head', 'img': 'https://user-images.githubusercontent.com/42682370/75374854-03aa2180-58f3-11ea-871c-3447e3a14442.jpg' },
+    { id: 2, 'name': 'Tanmay Ambadkar', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75517524-ed03e780-5a24-11ea-8eb7-94ab3821a0d7.jpg' }
   ],
 
   'Publicity and Marketing Team': [
-    { id: 1, 'name': 'Aayush Dheemar', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Anubhav Madhav', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 3, 'name': 'Anagha Mittal', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' }
+    { id: 1, 'name': 'Aayush Dheemar', 'role': 'Head', 'img': 'https://user-images.githubusercontent.com/42682370/75517534-f3925f00-5a24-11ea-99e0-874b41b2b95f.jpg' },
+    { id: 2, 'name': 'Anubhav Madhav', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75517546-f7be7c80-5a24-11ea-9c56-a88b8c3e9e0c.jpg' },
+    { id: 3, 'name': 'Anagha Mittal', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75374762-d9f0fa80-58f2-11ea-98a2-5051471f73f9.jpg' }
   ],
 
   'Gaming Team': [
-    { id: 1, 'name': 'Ravi Varshney', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Shivam Nigam', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' }
+    { id: 1, 'name': 'Shivam Nigam', 'role': 'Head', 'img': 'https://user-images.githubusercontent.com/42682370/75524629-4cb5bf00-5a34-11ea-9db1-2ab06bb7c7ee.jpg' },
+    { id: 2, 'name': 'Ravi Varshney', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75374763-da899100-58f2-11ea-8ece-cc714e2f5d6f.jpg' }
   ],
 
   'Art & Infrastructure Team': [
-    { id: 1, 'name': 'Srishti Chaudhary', 'role': 'Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 2, 'name': 'Shilp Khatri', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 3, 'name': 'Jinal Adhyaru', 'role': 'Co-Head', 'img': 'http://unsplash.it/230/230' },
-    { id: 3, 'name': 'Prateek Chouhan', 'role': 'Member', 'img': 'http://unsplash.it/230/230' },
-    { id: 4, 'name': 'Saksham Jain', 'role': 'Member', 'img': 'http://unsplash.it/230/230' }
-  ],
-
+    { id: 1, 'name': 'Srishti Chaudhary', 'role': 'Head', 'img': 'https://user-images.githubusercontent.com/42682370/75374755-d78ea080-58f2-11ea-9070-0cc853ca0942.jpg' },
+    { id: 2, 'name': 'Shilp Khatri', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75524649-55a69080-5a34-11ea-9126-941cf0a9341b.jpg' },
+    { id: 3, 'name': 'Jinal Adhyaru', 'role': 'Co-Head', 'img': 'https://user-images.githubusercontent.com/42682370/75524694-6a832400-5a34-11ea-9459-a30c260e075a.jpg' },
+    { id: 4, 'name': 'Saloni Lakhotia', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75524720-7838a980-5a34-11ea-8584-2f1b1cb2c3a9.jpg' },
+    { id: 5, 'name': 'Saksham Jain', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75524726-7a9b0380-5a34-11ea-8a2b-9b5d22d52e12.jpg' },
+    { id: 6, 'name': 'Varun Gupta', 'role': 'Member', 'img': 'https://user-images.githubusercontent.com/42682370/75524996-09a81b80-5a35-11ea-994e-fdf983727d98.png' }
+  ]
 }
