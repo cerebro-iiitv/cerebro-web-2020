f47ea7d2188554183bf0a108c821e4ef62234cc0
Vraj Mistry <vrajparesh22@gmail.com>
2020-02-29 18:28:55 +0530

    Footer Links updated with new tab

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -34,7 +34,7 @@ function Footer() {
 						<h2>Contact</h2>
 						<div className="footer_l__hr_text"><hr color="#29abe2" /></div><br /><br /><br /></div>
 				</div>
-					<div className="footer_l__contact_content"><a href='mailto:cerebro@iiitvadodara.ac.in' className="footer_l__contact_cn" ><b>Email</b>: cerebro@iiitvadodara.ac.in                             </a></div>
+					<div className="footer_l__contact_content"><a href='mailto:cerebro@iiitvadodara.ac.in' target="_blank" className="footer_l__contact_cn" ><b>Email</b>: cerebro@iiitvadodara.ac.in                             </a></div>
 					<div className="footer_l__contact_content"><a href='tel:8875038111' className="footer_l__contact_cn" ><b>Phone</b>: +91 8875038111,</a><a href='tel:9453286926' className="footer_l__contact_cn" > 9453286926  </a></div>
 					<div className="footer_l__contact_content"><a href='https://goo.gl/maps/7g5D6XJpMAD2' target="_blank" className="footer_l__contact_cn" ><b>Address</b>: c/o Block No.9, Government Engineering<div className="footer_l__contact_gmap">College, Sector-28, Gandhinagar, Gujarat - 382028</div></a></div>
 			</div>
